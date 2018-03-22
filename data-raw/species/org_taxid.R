74355fff2178c90a99d4c180aa0a19af5c4224df
callahantiff <callahantiff@gmail.com>
2018-03-22 10:14:41 -0600

    updated code formatting
    Removed reference to non-existent data

--- data-raw/species/org_taxid.R
+++ data-raw/species/org_taxid.R
@@ -2,8 +2,9 @@ library(GenomeInfoDbData)
 data(speciesMap)
 
 # crossmeta can get gene symbols for species in org_pkg or ens_spcs
-load("~/Documents/Batcave/GEO/crossmeta/R/sysdata.rda")
-ens_spcs <- readRDS('/home/alex/Documents/Batcave/GEO/SRAdb/ens_spcs.rds')
+load("~/Dropbox/GraduateSchool/PhD/LabWork/MetaOmic/transcriptomic/R/crossmeta/R/sysdata.rda")
+# ens_spcs <- readRDS('/home/alex/Documents/Batcave/GEO/SRAdb/ens_spcs.rds')
+ens_spcs <- list()
 
 # already have taxon ids for species in ens_spcs
 species <- setdiff(names(org_pkg), ens_spcs$scientific_name)
@@ -11,7 +12,7 @@ species <- setdiff(names(org_pkg), ens_spcs$scientific_name)
 speciesMap <- speciesMap[speciesMap$species %in% species, ]
 row.names(speciesMap) <- speciesMap$species
 
-org_taxid <- c(ens_spcs$taxon_id, speciesMap[species, 'taxon'])
-names(org_taxid) <- c(ens_spcs$scientific_name, species)
+org_taxid <- c(speciesMap$taxon, speciesMap[species, 'taxon'])
+names(org_taxid) <- c(speciesMap$species, species)
 
-devtools::use_data(gpl_bioc, homologene, sources, token, org_pkg, org_taxid, internal = TRUE, overwrite = TRUE)
+# devtools::use_data(gpl_bioc, homologene, sources, token, org_pkg, org_taxid, internal = TRUE, overwrite = TRUE)
